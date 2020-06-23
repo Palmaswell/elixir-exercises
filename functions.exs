f78f0713425e8d72b6b09731a7a843fd640df46a
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-06-23 22:44:50 +0200

    patten matching exercise

--- functions.exs
+++ functions.exs
@@ -5,7 +5,11 @@
 # If the first is 0 return "Fizz" 
 # if the second is 0 return "Buzz"
 # Otherwise return the third argument
-
+fizzbuzz = fn
+    {0, 0, _} -> "FizzBuzz"
+    {0, _, _} -> "Fizz"
+    {_, _, third} -> third
+end
 
 # Write a funtion that accepts a string and
 # and returns a function that accepts a string
@@ -13,5 +17,7 @@
 prefix = fn str -> fn str_2 -> "#{str} #{str_2}" end end
 mrs = prefix.("Mrs.")
 
-
-IO.puts mrs.("daisy")
+IO.puts mrs.("Daisy")
+IO.puts fizzbuzz.({0, 0, 1})
+IO.puts fizzbuzz.({0, 1, 1})
+IO.puts fizzbuzz.({1, 1, "surprice"})
