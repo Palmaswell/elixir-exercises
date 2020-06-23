f45e2a255e4241d4908eab2a4cd4a31b9ef6308b
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-06-23 22:32:41 +0200

    include anonymous functions exercises

---
+++ functions.exs
@@ -0,0 +1,17 @@
+
+# 1. 
+# Write a function that accepts three arguments
+# If the first two are 0 return "FizzBuzz"
+# If the first is 0 return "Fizz" 
+# if the second is 0 return "Buzz"
+# Otherwise return the third argument
+
+
+# Write a funtion that accepts a string and
+# and returns a function that accepts a string
+# and return both strings together
+prefix = fn str -> fn str_2 -> "#{str} #{str_2}" end end
+mrs = prefix.("Mrs.")
+
+
+IO.puts mrs.("daisy")
