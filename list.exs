e52c7d937090aa00fdbb3e4365445f849280cd6a
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-12-14 20:59:02 +0100

    feat: implement list span function

--- list.exs
+++ list.exs
@@ -1,6 +1,6 @@
 defmodule MyList do
-  def mapsum(_, [], val), do: val
-  def mapsum(func, [head | tail], val \\ 0) do
+  def mapsum(_, [], val \\ 0), do: val
+  def mapsum(func, [head | tail], val) do
      mapsum(func, tail, func.(head) + val)
   end
 
@@ -10,5 +10,16 @@ defmodule MyList do
   end
   def maxVal([head | tail], val) when head < val do
     maxVal(tail, val)
+  end
+
+  def ceasar([], _), do: []
+  def ceasar([head | tail], n) when head+9 <= ?z do
+    [head + n | ceasar(tail, n)]
+  end
+  def ceasar([head | tail], n), do: [head+n-26 | ceasar(tail, n)]
+
+  def span(from, to) when from > to, do: []
+  def span(from, to) do
+    [from | span(from + 1, to)]
   end
 end
